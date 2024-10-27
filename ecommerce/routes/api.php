b7ae9180a03d0a520450b7d810431a12ab6cde5c
johnvincentandaya <johnalmendral1@gmail.com>
2024-10-27 19:02:20 +0800

    search product

--- ecommerce/routes/api.php
+++ ecommerce/routes/api.php
@@ -9,5 +9,6 @@ Route::post('/products', [ProductController::class, 'store']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::put('/products/{id}', [ProductController::class, 'update']);
 Route::delete('/products/{id}', [ProductController::class, 'destroy']);
-Route::get('/products/search', [ProductController::class, 'search']); //search
+Route::get('/products/search', [ProductController::class, 'search']);
 
+ 
